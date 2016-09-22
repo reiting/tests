deef76bfae784864d4c5b34f7cea05d1f7fb2931
Rachel Eiting <racheleiting@Rachels-MacBook-Pro.local>
2016-09-22 18:17:02 -0500

    resolved some problems

--- happenTemplate.js
+++ happenTemplate.js
@@ -1,86 +1,155 @@
 var library = (function() {
+	var monthNumber = new Date().getMonth() + 1;
+	var Year = new Date().getFullYear(); 
+	
+
   return {
 	TimeStamp: (function(){
    	  return {
-		UnixTimestamp: function(){},
-		UnixMillisecond: function(){}
+		UnixTimestamp: function(){
+
+		},
+		UnixMillisecond: function(){
+
+		}
 	  }
 	})(),
 	Local: (function(){
 	  return {
 		Time: (function() {
 		  return {
-	  	    WithSeconds: function(){},
-	   	    WithOutSeconds: function() {}
+	  	    WithSeconds: function(){
+
+					},
+	   	    WithOutSeconds: function() {
+
+					 }
 		  }
 		})(),
 		MDY: (function(){
 	  	  return {
-		    Numeral: function(){},
-			Name: function(){}
+		    Numeral: function(){
+
+				},
+			Name: function(){
+
+			}
 		  }
 		  })(),
 		}
 	})(),
 	Second: (function(){
 		return{
-			Second: function(){},
-			DblDigit: function(){}
+			Second: function(){
+
+			},
+			DblDigit: function(){
+
+			}
 		}
 	})(),
 	Minute: (function(){
 		return{
-			Minute: function(){},
-			DblDigit: function(){}
+			Minute: function(){
+				
+			},
+			DblDigit: function(){
+
+			}
 		}
 	})(),
 	Hour: (function(){
 		return {
-			TwentyFourHour: function() {},
-			TwelveHour: function() {},
+			TwentyFourHour: function() {
+
+			},
+			TwelveHour: function() {
+
+			},
 			AMPM: (function() {
 				return {
-					UpperCase: function(){},
-					LowerCase: function(){}
+					UpperCase: function(){
+
+					},
+					LowerCase: function(){
+
+					}
 				}
 			})()
 		}
 	})(),
 	Week: (function(){
 		return {
-			DayOfWeek: function(){},
-			AbrDayOfWeek: function(){},
-			FirstTwoOfWeek: function(){},
-			WeekOfYear: function(){}
+			DayOfWeek: function(){
+
+			},
+			AbrDayOfWeek: function(){
+
+			},
+			FirstTwoOfWeek: function(){
+
+			},
+			WeekOfYear: function(){
+
+			}
 		}
 	})(),
 	Month: (function(){
 		return {
 			DateOfMonth: (function(){
 				return {
-					Numeral: function(){},
-					Ordinal: function(){},
-					DateDblDigit: function(){}
+					Numeral: function(){
+
+					},
+					Ordinal: function(){
+
+					},
+					DateDblDigit: function(){
+
+					}
 				}
 			})(),
-			MonthNumber: function(){},
-			MonthNumberDblDigit: function(){},
-			AbrOfCurrentMonth: function(){},
-			CurrentMonth: function(){}
+			MonthNumber: function(){
+				return monthNumber.toString();
+			},
+			MonthNumberDblDigit: function(){
+				if (monthNumber < 10) {
+					return ("0" + monthNumber);
+				} else {
+					return monthNumber;
+				}
+			},
+			AbrOfCurrentMonth: function(){
+			
+			},
+			CurrentMonth: function(){
+				
+			}
 		}
 	})(),
 	Year: (function(){
 		return {
 			DayOfYear: (function(){
 				return {
-					Numeral: function(){},
-					Ordinal: function(){}
+					Numeral: function(){
+
+					},
+					Ordinal: function(){
+
+					}
 				}
 			})(),
-			YearFull: function(){},
-			YearAbr: function(){}
+			YearFull: function(){
+				return Year.toString();
+				
+			},
+			YearAbr: function(){
+				return (Year % 100).toString();
+			}
 		}
 	})(),
-	Defaults: function(){}
+	Defaults: function(){
+
+	}
   }
 })();
