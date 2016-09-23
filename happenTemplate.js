1f69b3d01ae7aa231e8879e7363699ee00a54f21
Rachel Eiting <racheleiting@Rachels-MacBook-Pro.local>
2016-09-22 19:23:34 -0500

    8 passes

--- happenTemplate.js
+++ happenTemplate.js
@@ -1,7 +1,13 @@
 var library = (function() {
 	var monthNumber = new Date().getMonth() + 1;
 	var Year = new Date().getFullYear(); 
-	
+	var dayMonth = new Date().getDate();
+	vardayYear = new Date().get
+	var monthNames = ["January", "February", "March", "April", "May", "June",
+ 	 "July", "August", "September", "October", "November", "December"];
+	var currentMonth = (monthNames [new Date().getMonth()]);
+	var abrMonthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+	var abrCurrentMonth = (abrMonthNames [new Date().getMonth()]);
 
   return {
 	TimeStamp: (function(){
@@ -99,13 +105,17 @@ var library = (function() {
 			DateOfMonth: (function(){
 				return {
 					Numeral: function(){
-
+						return dayMonth.toString();
 					},
 					Ordinal: function(){
 
 					},
 					DateDblDigit: function(){
-
+						if (dayMonth < 10) {
+							return ("0" + dayMonth.toString());
+						} else {
+							return dayMonth.toString();
+						}
 					}
 				}
 			})(),
@@ -114,16 +124,16 @@ var library = (function() {
 			},
 			MonthNumberDblDigit: function(){
 				if (monthNumber < 10) {
-					return ("0" + monthNumber);
+					return ("0" + monthNumber.toString());
 				} else {
-					return monthNumber;
+					return monthNumber.toString();
 				}
 			},
 			AbrOfCurrentMonth: function(){
-			
+				return abrCurrentMonth;
 			},
 			CurrentMonth: function(){
-				
+				return currentMonth;
 			}
 		}
 	})(),
